14283c786adb800d7cf023a82abd9a33132fc3c0
kenzasanaa <kenzasanaa00@gmail.com>
2026-07-01 16:35:50 +0100

    fix

--- src/form/SignInPage.jsx
+++ src/form/SignInPage.jsx
@@ -37,7 +37,8 @@ export default function SignInPage() {
     const payload = { email, password };
 
     try {
-      const response = await fetch('http://localhost:5000/api/signin', {
+      const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+      const response = await fetch(`${API_URL}/api/signin`,  {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
